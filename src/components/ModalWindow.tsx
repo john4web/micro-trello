66ede46a95b733bf1912208b7e36a6bcd2ddb585
Miriam Hager <miriam.hager@gmx.at>
2021-03-31 16:42:38 +0200

    Modalwindow create btn lable changes depending on type

--- src/components/ModalWindow.tsx
+++ src/components/ModalWindow.tsx
@@ -30,7 +30,7 @@ export const ModalWindow = ({ closeModal, type }: IProps) => {
           }}
           className="h-10 px-5 m-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800"
         >
-          Create new Project
+          Create new {type}
         </button>
       </div>
     </div>
