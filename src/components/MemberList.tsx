f91628674853a5a5e050b172b62353bbfca1a33c
Miriam Hager <miriam.hager@gmx.at>
2021-04-09 22:38:03 +0200

    added dropdown to members and delete funcionality

--- src/components/MemberList.tsx
+++ src/components/MemberList.tsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
 import { Fragment } from "react";
+import { DropDownMenu } from "./DropDownMenu";
 
 export const MemberList = () => {
   const { members } = useSelector((state: RootState) => state.member);
@@ -9,12 +10,15 @@ export const MemberList = () => {
     <Fragment>
       {members.map((member, index) => {
         return (
-          <ul key={index} className=" m-4 border-black border-2">
-            <li>{member.firstname}</li>
-            <li>{member.lastname}</li>
-            <li>{member.job}</li>
-            <li>{member.skill}</li>
-          </ul>
+          <div className=" m-4 border-black border-2">
+            <DropDownMenu type="member" item={member} />
+            <ul key={index}>
+              <li>{member.firstname}</li>
+              <li>{member.lastname}</li>
+              <li>{member.job}</li>
+              <li>{member.skill}</li>
+            </ul>
+          </div>
         );
       })}
     </Fragment>
