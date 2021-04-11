82f4c1f1e377caf0fa3107fd48574c268fbcc1a7
Johannes Gerstbauer <johannes.gerstbauer@gmail.com>
2021-04-11 18:46:27 +0200

    removing column slice and task slice since they are not used

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,11 @@
 import { combineReducers, createStore } from "@reduxjs/toolkit";
 import memberReducer from "./memberSlice";
 import projectReducer from "./projectSlice";
-import taskReducer from "./taskSlice";
-import columnReducer from "./columnSlice";
 import { loadLocalStorage } from "./localStorage";
 
 const reducers = combineReducers({
   member: memberReducer,
   project: projectReducer,
-  task: taskReducer,
-  column: columnReducer,
 });
 const persitentState = loadLocalStorage();
 export const store = createStore(reducers, persitentState);
