e3cc7998f78066e9337a8f70cbbab41a7364fd50
Miriam Hager <miriam.hager@gmx.at>
2021-04-14 20:45:09 +0200

    unit test for members are working

--- src/tests/Member.test.tsx
+++ src/tests/Member.test.tsx
@@ -34,21 +34,34 @@ describe("Member Component", () => {
 
   test("addMembers", () => {
     testStore.dispatch(addMember(member1));
+    expect(testStore.getState().member.members.length).toEqual(1);
     testStore.dispatch(addMember(member2));
     expect(testStore.getState().member.members.length).toEqual(2);
   });
 
   test("updateMember", () => {
     const member2update: Member = {
-      id: "m2",
+      id: testStore.getState().member.members[1].id,
       firstname: "Max",
-      lastname: "Huber",
-      job: "Test member component",
-      skill: "No skill",
+      lastname: "Mustermann",
+      job: "Test update member component",
+      skill: "webdev pro",
       photo: "",
     };
+    testStore.dispatch(updateMember(member2update));
+    expect(testStore.getState().member.members[1].firstname).toEqual("Max");
+    expect(testStore.getState().member.members[1].lastname).toEqual(
+      "Mustermann"
+    );
+    expect(testStore.getState().member.members[1].job).toEqual(
+      "Test update member component"
+    );
+    expect(testStore.getState().member.members[1].skill).toEqual("webdev pro");
+  });
 
-    // testStore.dispatch(updateMember(member2update));
-    //  expect(testStore.getState().member.members[1].firstname).toEqual("Max");
+  test("removeMember", () => {
+    testStore.dispatch(removeMember(testStore.getState().member.members[0].id));
+    expect(testStore.getState().member.members.length).toEqual(1);
+    expect(testStore.getState().member.members[0].firstname).toEqual("Max");
   });
 });
